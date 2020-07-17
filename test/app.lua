763aa47641436417e3a12e07c32fc57752b94b96
rong <1141591465@qq.com>
2020-07-17 10:56:48 +0800

    add more dcos

---
+++ test/app.lua
@@ -0,0 +1,16 @@
+-- local uri = ngx.re.sub("/index/1", [[^(.*?)(.html)?\/1$]], "/demo1$1", "o")
+-- print(uri)
+
+local origin_url = [[/dalong/dalong]]
+local uri,n,err = ngx.re.sub(origin_url, [[^(.*?)(.html)?\/(\d)$]], "/demo$3$1", "o")
+-- local uri = ngx.re.sub("/index/1", [[^(.*?)(.html)?\/(\d)$]], "/demo$3$1", "o")
+
+if n ==0 then
+  print("not sub")
+end
+
+local m, err = ngx.re.match(origin_url,[[^(.*?)(.html)?\/(\d)$]])
+if not m  then 
+    print("not match")
+end
+
